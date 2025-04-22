3ea05feae9ed320207fae34fdaf9d38dd1bdd4dd
Angellsworth <angellsworth@msn.com>
2025-04-22 13:37:51 -0600

    added models

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     #will hold all the routes here
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('cats/', views.cat_index, name='cat_index'),
+
 ]
 
--- main_app/views.py
+++ main_app/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse
 def home(request):
     #each view function or "view"
     #recieves a request object
-    return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+    return render(request, 'home.html')
 #to send a response, we return it
 
 def about(request):
@@ -15,3 +15,24 @@ def about(request):
     return render(request, 'about.html', {
         'contact': contact_details
     })
+
+# views.py
+
+class Cat:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+# Create a list of Cat instances
+cats = [
+    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
+    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
+    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
+
+
+def cat_index(request):
+    return render(request, 'cats/index.html', {'cats': cats})
