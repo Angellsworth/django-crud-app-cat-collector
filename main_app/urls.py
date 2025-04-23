79b39c8535aa323eeb59d463cf2a4f3864eb40b6
Angellsworth <angellsworth@msn.com>
2025-04-23 11:21:10 -0600

    adding everything problems with memory

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    #will hold all the routes here
     path('about/', views.about, name='about'),
-    path('cats/', views.cat_index, name='cat_index'),
+    path('cats/', views.cat_index, name='cat-index'),
     path('cats/<int:cat_id>/', views.cat_detail, name='cat-detail'),
+    path('cats/create/', views.CatCreate.as_view(), name='cat-create'),
 ]
 
