70f33aae88d62532626d6f0eb27c350a0f274bab
Angellsworth <angellsworth@msn.com>
2025-04-24 17:11:40 -0600

    wip: fixed base.html

--- catcollector/settings.py
+++ catcollector/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "a7u!^o#+6=2p$0@%%wzpos03y6#9*527x-p4pjxjrj%vhh9ld7"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
